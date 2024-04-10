93406bcfb1247c369f80752c914beb14cd664f5b
PandaHajs <kacpermariaskowronski@protonmail.com>
2024-04-10 14:22:10 +0200

    changed icon type from any to string

--- src/app/models/page.tsx
+++ src/app/models/page.tsx
@@ -7,36 +7,36 @@ import { Suspense } from "react";
 import { FBXLoader } from "three/examples/jsm/Addons.js";
 
 export default function Models() {
-  const Model = () => {
-    const fbx = useLoader(FBXLoader, "/diorama.fbx");
-    return (
-      <group>
-        <primitive scale={0.02} object={fbx} />
-      </group>
-    );
-  };
+	const Model = () => {
+		const fbx = useLoader(FBXLoader, "/diorama.fbx");
+		return (
+			<group>
+				<primitive scale={0.02} object={fbx} />
+			</group>
+		);
+	};
 
-  const ModelCanvas = () => {
-    return (
-      <Canvas camera={{ position: [0, 5, 5] }}>
-        <ambientLight intensity={50} />
-        <Suspense fallback={null}>
-          <Model />
-        </Suspense>
-        <OrbitControls />
-      </Canvas>
-    );
-  };
+	const ModelCanvas = () => {
+		return (
+			<Canvas camera={{ position: [0, 5, 5] }}>
+				<ambientLight intensity={50} />
+				<Suspense fallback={null}>
+					<Model />
+				</Suspense>
+				<OrbitControls />
+			</Canvas>
+		);
+	};
 
-  return (
-    <div className={styles.main}>
-      <section className={styles.menu}>
-        <h1>Models</h1>
-        <p>Here are some 3D models</p>
-      </section>
-      <div className={styles.model}>
-        <ModelCanvas />
-      </div>
-    </div>
-  );
+	return (
+		<div className={styles.main}>
+			<section className={styles.menu}>
+				<h1>Models</h1>
+				<p>Here are some 3D models</p>
+			</section>
+			<div className={styles.model}>
+				<ModelCanvas />
+			</div>
+		</div>
+	);
 }
