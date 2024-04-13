4fbf3d9fd13f98c36b07283a083b7223e6696577
PandaHajs <kacpermariaskowronski@protonmail.com>
2024-04-13 19:03:39 +0200

    remade gallery logic

--- src/app/ui/gallery.tsx
+++ src/app/ui/gallery.tsx
@@ -4,6 +4,7 @@ import type { galleryProps } from "@/app/lib/types";
 import { shimmer, toBase64 } from "@/app/lib/galleryLogic";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
+import Masonry from "react-responsive-masonry";
 
 export default function Gallery(props: galleryProps) {
 	const images = props.images;
@@ -22,117 +23,39 @@ export default function Gallery(props: galleryProps) {
 
 	return (
 		<section className={styles.gallery}>
-			<div className={styles.line}>
-				{images
-					.filter(
-						(image) =>
-							Number.parseInt(image.id) % 3 === 1 && image.tag === props.tag,
-					)
-					.map((image) => (
-						<div
-							role="button"
-							tabIndex={check}
-							onClick={() => {
+			<Masonry columnsCount={3} gutter="1em">
+				{images.map((image) => (
+					<div
+						role="button"
+						tabIndex={check}
+						onClick={() => {
+							router.push(`?id=${image.id}`, { scroll: false });
+						}}
+						onKeyDown={(e) => {
+							if (e.key === "Enter")
 								router.push(`?id=${image.id}`, { scroll: false });
+						}}
+						key={image.id}
+						className={styles.galleryImage}
+					>
+						<Image
+							priority={Number.parseInt(image.id) <= 3 ? true : false}
+							src={image.src}
+							alt={image.alt}
+							width={image.width}
+							height={image.height}
+							style={{
+								width: "100%",
+								height: "auto",
 							}}
-							onKeyDown={(e) => {
-								if (e.key === "Enter")
-									router.push(`?id=${image.id}`, { scroll: false });
-							}}
-							key={image.id}
-						>
-							<Image
-								priority={Number.parseInt(image.id) <= 3 ? true : false}
-								src={image.src}
-								alt={image.alt}
-								width={image.width}
-								height={image.height}
-								style={{
-									width: "100%",
-									height: "auto",
-								}}
-								quality={40}
-								placeholder={`data:image/svg+xml;base64,${toBase64(
-									shimmer(700, 475),
-								)}`}
-							/>
-						</div>
-					))}
-			</div>
-			<div className={styles.line}>
-				{images
-					.filter(
-						(image) =>
-							Number.parseInt(image.id) % 3 === 2 && image.tag === props.tag,
-					)
-					.map((image) => (
-						<div
-							role="button"
-							tabIndex={check}
-							onClick={() => {
-								router.push(`?id=${image.id}`, { scroll: false });
-							}}
-							onKeyDown={(e) => {
-								if (e.key === "Enter")
-									router.push(`?id=${image.id}`, { scroll: false });
-							}}
-							key={image.id}
-						>
-							<Image
-								priority={Number.parseInt(image.id) <= 3 ? true : false}
-								src={image.src}
-								alt={image.alt}
-								width={image.width}
-								height={image.height}
-								style={{
-									width: "100%",
-									height: "auto",
-								}}
-								quality={40}
-								placeholder={`data:image/svg+xml;base64,${toBase64(
-									shimmer(700, 475),
-								)}`}
-							/>
-						</div>
-					))}
-			</div>
-			<div className={styles.line}>
-				{images
-					.filter(
-						(image) =>
-							Number.parseInt(image.id) % 3 === 0 && image.tag === props.tag,
-					)
-					.map((image) => (
-						<div
-							role="button"
-							tabIndex={check}
-							onClick={() => {
-								router.push(`?id=${image.id}`, { scroll: false });
-							}}
-							onKeyDown={(e) => {
-								if (e.key === "Enter")
-									router.push(`?id=${image.id}`, { scroll: false });
-							}}
-							key={image.id}
-						>
-							<Image
-								priority={Number.parseInt(image.id) <= 3 ? true : false}
-								src={image.src}
-								alt={image.alt}
-								width={image.width}
-								height={image.height}
-								style={{
-									width: "100%",
-									height: "auto",
-								}}
-								quality={40}
-								placeholder={`data:image/svg+xml;base64,${toBase64(
-									shimmer(700, 475),
-								)}`}
-							/>
-						</div>
-					))}
-			</div>
+							quality={40}
+							placeholder={`data:image/svg+xml;base64,${toBase64(
+								shimmer(700, 475),
+							)}`}
+						/>
+					</div>
+				))}
+			</Masonry>
 		</section>
 	);
 }
