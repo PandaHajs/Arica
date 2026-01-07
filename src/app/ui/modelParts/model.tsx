c49fd0cb2a25609a48ea0788c312bc7f125dd724
Kacper Skowroński <kacpermariaskowronski@protonmail.com>
2026-01-07 16:20:24 +0100

    graphic rehaul

--- src/app/ui/modelParts/model.tsx
+++ src/app/ui/modelParts/model.tsx
@@ -5,36 +5,41 @@ import { useLoader } from "@react-three/fiber";
 import { Suspense } from "react";
 import { FBXLoader } from "three/examples/jsm/Addons.js";
 import styles from "../styles/model.module.scss";
-import { useSearchParams, useRouter } from "next/navigation";
+import { useSearchParams } from "next/navigation";
 
 export default function Model() {
-	const searchParams = useSearchParams();
-	const modelID = searchParams.get("modelID");
+  const searchParams = useSearchParams();
+  const rawModelID = searchParams.get("modelID");
+  const modelID =
+    typeof rawModelID === "string" && /^\d+$/.test(rawModelID)
+      ? rawModelID
+      : "1";
+  const modelUrl = `/${modelID}.fbx`;
 
-	const Model3D = () => {
-		const fbx = useLoader(FBXLoader, `${modelID}.fbx`);
-		return (
-			<group>
-				<primitive scale={0.02} object={fbx} />
-			</group>
-		);
-	};
+  const Model3D = () => {
+    const fbx = useLoader(FBXLoader, modelUrl);
+    return (
+      <group>
+        <primitive scale={0.02} object={fbx} />
+      </group>
+    );
+  };
 
-	const ModelCanvas = () => {
-		return (
-			<Canvas camera={{ position: [0, 5, 5] }}>
-				<ambientLight intensity={50} />
-				<Suspense fallback={null}>
-					<Model3D />
-				</Suspense>
-				<CameraControls />
-			</Canvas>
-		);
-	};
+  const ModelCanvas = () => {
+    return (
+      <Canvas camera={{ position: [0, 5, 5] }}>
+        <ambientLight intensity={50} />
+        <Suspense fallback={null}>
+          <Model3D />
+        </Suspense>
+        <CameraControls />
+      </Canvas>
+    );
+  };
 
-	return (
-		<div className={styles.model}>
-			<ModelCanvas />
-		</div>
-	);
+  return (
+    <div className={styles.model}>
+      <ModelCanvas />
+    </div>
+  );
 }
